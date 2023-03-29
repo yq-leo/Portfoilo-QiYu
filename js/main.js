dd287b784d32fe81ab95cf2392a9432edbf1b7c5
qiyu6 <qiyu6@illinois.edu>
2023-03-29 00:58:24 -0500

    edit nav bar toggle effect

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
-  })
-);
+// collapsibles.forEach((item) =>
+//   item.addEventListener("click", function () {
+//     this.classList.toggle("collapsible--expanded");
+//   })
+// );
+
+function expandNavBar() {
+  collapsibles.forEach((item) => {
+      item.classList.toggle("collapsible--expanded");
+  });
+}
 
 function alertSubmitSuccess() {
   alert("Submitted! Thank you for your message!");
