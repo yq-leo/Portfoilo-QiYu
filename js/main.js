007e375c867ae89fabe61c722cd9f36fe1d818f7
qiyu6 <qiyu6@illinois.edu>
2023-11-09 17:57:19 -0600

    feat: add deprecated message

--- js/main.js
+++ js/main.js
@@ -7,10 +7,14 @@ const collapsibles = document.querySelectorAll(".collapsible");
 
 function expandNavBar() {
   collapsibles.forEach((item) => {
-      item.classList.toggle("collapsible--expanded");
+    item.classList.toggle("collapsible--expanded");
   });
 }
 
 function alertSubmitSuccess() {
   alert("Submitted! Thank you for your message!");
 }
+
+alert(
+  "This website is deprecated and no longer maintained. Please visit https://yq-leo.github.io/portfolio/ (still under construction) for the latest version with some exciting new features!"
+);
